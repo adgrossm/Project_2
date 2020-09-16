7111f97d56cd951e44b068dcd2cad95b43198f83
Adam Grossman <adgrossm@gmail.com>
2020-09-16 11:27:15 -0400

    setting up artist sign up page and table

---
+++ models/artist.js
@@ -0,0 +1,18 @@
+// Creating our User model
+module.exports = function(sequelize, DataTypes) {
+    const Artist = sequelize.define("Artist", {
+      // The email cannot be null, and must be a proper email before creation
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      // The password cannot be null
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+
+      user_id: {
+        type: 
+      }
+    });
