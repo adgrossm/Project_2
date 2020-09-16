17e92fde6511f2c77781e43ed9530340b3f92919
Adam Grossman <adgrossm@gmail.com>
2020-09-16 12:12:39 -0400

    set up artist.js genre.js instrument.js files still need to test

--- models/artist.js
+++ models/artist.js
@@ -1,19 +1,41 @@
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
-    const Artist = sequelize.define("Artist", {
-      // The email cannot be null, and must be a proper email before creation
-      first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      // The password cannot be null
-      last_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
+  const Artist = sequelize.define("Artist", {
+    // The email cannot be null, and must be a proper email before creation
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // The password cannot be null
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
 
-      user_id: {
-        type: 
-        
+  Artist.associate = models => {
+    Artist.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
       }
     });
+  };
+
+  Artist.associate = models => {
+    Artist.belongsTo(models.Instrument, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  Artist.associate = models => {
+    Artist.belongsTo(models.Genre, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Artist;
+};
