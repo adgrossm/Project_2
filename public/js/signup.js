a91705857c8a40b41297ddfe86e98f626ec65a0a
Adam Grossman <adgrossm@gmail.com>
2020-09-16 21:07:45 -0400

    adding const to signup.js file to start pulling user inputs

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,12 @@
 $(document).ready(() => {
   // Getting references to our form and input
   const signUpForm = $("form.signup");
-  const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
+  // const emailInput = $("input#email-input");
+  // const passwordInput = $("input#password-input");
+
+  const firstNameInput = $("input#orangeForm-nameF");
+  const lastNameInput = $("input#orangeForm-nameL");
+  const genreInput = $("")
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
