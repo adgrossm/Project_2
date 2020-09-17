bc77b0808472194bb05c412eb23b2a47d9f665bd
ryanbrooks99 <ryanbrooks99@gmail.com>
2020-09-17 11:57:28 -0400

    added seed data for artists and user tables

--- seeds.sql
+++ seeds.sql
@@ -23,3 +23,13 @@ INSERT INTO genres (genre_name) VALUES ('R & B');
 INSERT INTO genres (genre_name) VALUES ('Punk');
 INSERT INTO genres (genre_name) VALUES ('Reggae');
 INSERT INTO genres (genre_name) VALUES ('Alternative');
+
+INSERT INTO artists (first_name, last_name, createdAt, updatedAt) VALUES ('Gino', 'Zaccardelli', now(), now());
+INSERT INTO artists (first_name, last_name, createdAt, updatedAt) VALUES ('Collin', 'Lanier', now(), now());
+INSERT INTO artists (first_name, last_name, createdAt, updatedAt) VALUES ('Adam', 'Grossman', now(), now());
+INSERT INTO artists (first_name, last_name, createdAt, updatedAt) VALUES ('Ryan', 'Brooks', now(), now());
+
+INSERT INTO users (email, password, createdAt, updatedAt) VALUES ('test@test.com', 'rootroot', now(), now());
+INSERT INTO users (email, password, createdAt, updatedAt) VALUES ('example@example.com', 'rootroot', now(), now());
+INSERT INTO users (email, password, createdAt, updatedAt) VALUES ('testing@testing.com', 'rootroot', now(), now());
+INSERT INTO users (email, password, createdAt, updatedAt) VALUES ('examples@examples.com', 'rootroot', now(), now());
